f4e6cd63ab47cd556fd5e4236726e9eecd70a354
AspieCoder <lukebraithwaite7@gmail.com>
2020-04-09 15:14:17 +0100

    Update bubble.py

--- Chapter 1/bubble.py
+++ Chapter 1/bubble.py
@@ -1,7 +1,9 @@
 def bubble(arr):
     print(arr)
+    # Traverses through all elements in the array
     for i in range(len(arr)):
-        for j in range(len(arr) - 1):
+        # Traverses through all elements not already sorted
+        for j in range(len(arr)-i - 1):
             if arr[j] > arr[j + 1]:
                 temp = arr[j]
                 arr[j] = arr[j + 1]
